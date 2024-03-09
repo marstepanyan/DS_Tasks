7a776de25e1490eaf9812bfe6b90e42ce2ac537d
marstepanyan <102877297+marstepanyan@users.noreply.github.com>
2024-03-09 18:16:48 +0400

    Add files via upload

---
+++ Day_1/GetTextWiki.py
@@ -0,0 +1,45 @@
+import requests
+import re
+
+
+def fetch_wikipedia_page(title):
+    if not title:
+        url = "https://en.wikipedia.org/wiki/Python_(programming_language)"
+    else:
+        url = f"https://en.wikipedia.org/wiki/{title}"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
+        return response.text
+    except requests.RequestException as e:
+        print(f"Error fetching Wikipedia page: {e}")
+        return None
+
+
+def extract_text(html_content):
+    pattern = r'<p>(.*?)<\/p>'
+    text = "\n".join(re.findall(pattern, html_content, re.DOTALL))  # Using re.DOTALL to match across newlines
+    return text
+
+
+def save_to_file(content, filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(content)
+    print(f"Content saved to {filename}")
+
+
+def main():
+    title = input("Enter the title of the Wikipedia page or skip and get info about Python: ")
+    content = fetch_wikipedia_page(title)
+    if content:
+        text_content = extract_text(content)
+        if title:
+            filename = f"{title.replace(' ', '_')}.txt"
+        else:
+            filename = "PythonProgLangWiki.txt"
+        save_to_file(text_content, filename)
+
+
+if __name__ == "__main__":
+    main()
